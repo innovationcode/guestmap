13bf24be1ca774b67469e0f129c4cfe3afe31f8b
lajawanti <lajawanti.dhake@gmail.com>
2019-12-17 23:52:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import L from 'leaflet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var myIcon = L.icon({
+  iconUrl: 'https://png.pngtree.com/png-clipart/20190516/original/pngtree-location-vector-icon-png-image_3722521.jpg',
+  iconSize: [25, 41],
+  iconAnchor: [12.5, 41],
+  popupAnchor:[0, -41]
+})
+
+class App extends Component {
+  state = {
+    location : {
+      lat: 51.505,
+      lng: -0.09,
+    },
+    haveUsersLocation: false,
+    zoom: 2,
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        location : {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        },
+        haveUsersLocation: true,
+        zoom: 13,
+      });
+    }, () => {
+      console.log("Location not provided.....")
+    });
+  }
+
+  render() {
+    const position = [this.state.location.lat, this.state.location.lng]
+
+    return (
+      <Map className = "map" center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {this.state.haveUsersLocation ?
+            <Marker position={position} icon = {myIcon}>
+              <Popup>
+                🌄 Atharva Dhake 😎 <br /> Tanishka 💦 🍠 🎉
+              </Popup>
+            </Marker> : ""
+        }
+      </Map>
+    );
+  }
 }
 
 export default App;
