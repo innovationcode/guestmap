94d5cc839babdbc3a1453675a3f9a2e1b6d05e80
lajawanti <lajawanti.dhake@gmail.com>
2019-12-18 12:23:47 -0500

    location using IP added and reactstrap and bootstrap started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
-import L from 'leaflet';
+import L, { divIcon } from 'leaflet';
 import './App.css';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+
 
 var myIcon = L.icon({
   iconUrl: 'https://png.pngtree.com/png-clipart/20190516/original/pngtree-location-vector-icon-png-image_3722521.jpg',
@@ -29,29 +31,51 @@ class App extends Component {
         },
         haveUsersLocation: true,
         zoom: 13,
-      });
-    }, () => {
-      console.log("Location not provided.....")
-    });
-  }
+      }); 
+    }, () => { //callback in navigator.geolocation if location not allowed then find using IP 
+             console.log("Location not provided...")
+             fetch('https://ipapi.co/json')
+                .then(res => res.json())
+                .then(location => {
+                    console.log(location);
+                      this.setState({
+                          location: {
+                              lat: location.latitude,
+                              lng: location.longitude
+                          },
+                          haveUsersLocation: true,
+                          zoom: 13,
+                      });
+                 })
+              } // callback end here 
+    ); //navigator.location end
+  } //componentDidmout end..
+  
 
   render() {
     const position = [this.state.location.lat, this.state.location.lng]
 
     return (
-      <Map className = "map" center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {this.state.haveUsersLocation ?
-            <Marker position={position} icon = {myIcon}>
-              <Popup>
-                🌄 Atharva Dhake 😎 <br /> Tanishka 💦 🍠 🎉
-              </Popup>
-            </Marker> : ""
-        }
-      </Map>
+      <div className = "map">
+        <Map className = "map" center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {this.state.haveUsersLocation ?
+              <Marker position={position} icon = {myIcon}>
+                <Popup>
+                  🌄 Atharva Dhake 😎 <br /> Tanishka 💦 🍠 🎉
+                </Popup>
+              </Marker> : ""
+          }
+        </Map>
+        <Card body>
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Go somewhere</Button>
+        </Card>
+      </div>  
     );
   }
 }
